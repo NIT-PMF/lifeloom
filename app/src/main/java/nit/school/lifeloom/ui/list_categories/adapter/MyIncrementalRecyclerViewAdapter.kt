a2354d0adb85eb70a609e77f9a10b686a7fafb7c
Irhad Fejzic <irhad.fejzic@gmail.com>
2020-07-05 16:49:27 +0200

    added fragments for categories

---
+++ app/src/main/java/nit/school/lifeloom/ui/list_categories/adapter/MyIncrementalRecyclerViewAdapter.kt
@@ -0,0 +1,53 @@
+package nit.school.lifeloom.ui.list_categories.adapter
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import nit.school.lifeloom.R
+
+import kotlinx.android.synthetic.main.fragment_incremental.view.*
+import nit.school.lifeloom.logic.showToast
+import nit.school.lifeloom.singleton.IncrementCategory
+
+class MyIncrementalRecyclerViewAdapter(
+    private val mValues: List<IncrementCategory>,
+    private val context: Context,
+    private val navController: NavController
+) : RecyclerView.Adapter<MyIncrementalRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_incremental, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = mValues[position]
+        holder.mIdView.text = item.name
+        holder.mContentView.text = item.description
+
+        holder.mButton.setOnClickListener { view ->
+            val bundle = bundleOf("activityName" to item.name)
+            navController.navigate(R.id.activityTrackerFragment, bundle)
+        }
+
+    }
+
+    override fun getItemCount(): Int = mValues.size
+
+    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val mIdView: TextView = mView.item_number
+        val mContentView: TextView = mView.content
+        val mButton: ImageButton = mView.inspect_btn
+
+        override fun toString(): String {
+            return super.toString() + " '" + mContentView.text + "'"
+        }
+    }
+}
