a17f10c395ae5482d13ca2dc17d9ea3b42c6936c
tarik11 <60644723+tarik11@users.noreply.github.com>
2020-07-09 23:05:10 +0200

    defaultActivity

--- app/src/main/java/nit/school/lifeloom/ui/adding/adapter/standardActivitiesAdapter.kt
+++ app/src/main/java/nit/school/lifeloom/ui/adding/adapter/standardActivitiesAdapter.kt
@@ -7,13 +7,28 @@ import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.standard_activity_list_item.view.*
+import kotlinx.coroutines.*
+import nit.school.lifeloom.MainActivity
 import nit.school.lifeloom.R
+import nit.school.lifeloom.database.AppDB
+import nit.school.lifeloom.database.dao.IncrementCategoryDao
+import nit.school.lifeloom.database.dao.QuantityCategoryDao
+import nit.school.lifeloom.database.dao.TimeCategoryDao
+import nit.school.lifeloom.database.entity.IncrementTable
+import nit.school.lifeloom.database.entity.QuantityTable
+import nit.school.lifeloom.database.entity.TimeTable
 import nit.school.lifeloom.logic.showToast
-import nit.school.lifeloom.singleton.Activity
+import nit.school.lifeloom.singleton.*
 import nit.school.lifeloom.ui.adding.adapter.StandardActivitiesAdapter.*
+import java.util.*
 
 
 class StandardActivitiesAdapter(private val activitesList: List<Activity>, private val context: Context, private val navController: NavController) :  RecyclerView.Adapter<StandardActivityViewHolder>() {
+    private lateinit var timeDb: TimeCategoryDao
+    private lateinit var quantityDb: QuantityCategoryDao
+    private lateinit var incrementDb: IncrementCategoryDao
+    private lateinit var job: Job
+    private lateinit var uiScope: CoroutineScope
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StandardActivityViewHolder {
         val itemView = LayoutInflater.from(parent.context)
@@ -26,14 +41,51 @@ class StandardActivitiesAdapter(private val activitesList: List<Activity>, priva
     }
 
 
+    @InternalCoroutinesApi
     override fun onBindViewHolder(holder: StandardActivityViewHolder, position: Int) {
+
+        timeDb = AppDB.getInstance((context as MainActivity).applicationContext).timeCategoryDao
+        quantityDb = AppDB.getInstance((context as MainActivity).applicationContext).quantityCategoryDao
+        incrementDb = AppDB.getInstance((context as MainActivity).applicationContext).incrementCategoryDao
+
+        job = Job()
+        uiScope = CoroutineScope(Dispatchers.Main + job)
 
         holder.name.text = activitesList[position].name
         holder.activityInfo.text = activitesList[position].description
         holder.add_btn.setOnClickListener { view ->
             //val bundle = bundleOf("activityName" to name.text.toString())
             //navController.navigate(R.id.activityTrackerFragment, bundle)
-            showToast(context, "DODAVANJE U BAZU")
+            if(activitesList[position].name == "Trcanje"){
+                quantitySingleton.addActivity(QuantityCategory(0, activitesList[position].name, "Kilometri koje sam trcao",  Calendar.getInstance(),
+                        mutableListOf(),0,"km"))
+                runBlocking { withContext(Dispatchers.IO){
+                    quantityDb.insert(QuantityTable( name = activitesList[position].name, description = "Kilometri koje sam trcao",  date = Calendar.getInstance().timeInMillis,
+                            properties = "",value = 0, unit= "km"))
+                } }
+            }else if(activitesList[position].name == "Skakanje"){
+                incrementSingleton.addActivity(IncrementCategory(0, activitesList[position].name, "Centimetri koje sam skocio",  Calendar.getInstance(),
+                        mutableListOf(),0,1))
+                runBlocking { withContext(Dispatchers.IO){
+                    incrementDb.insert(IncrementTable( name = activitesList[position].name, description = "Centimetri koje sam skocio",  date = Calendar.getInstance().timeInMillis,
+                            properties = "",value = 0, increment= 1))
+                } }
+            }else if(activitesList[position].name == "Spavanje"){
+                timePeriodSingleton.addActivity(TimeCategory(0, activitesList[position].name, "Sati koje sam spavao", Calendar.getInstance(), mutableListOf(), Calendar.getInstance(), Calendar.getInstance(), 0))
+                runBlocking { withContext(Dispatchers.IO){
+                    timeDb.insert(TimeTable( name = activitesList[position].name, description = "Sati koje sam spavao",  date = Calendar.getInstance().timeInMillis,
+                            properties = "", startTime = Calendar.getInstance().timeInMillis, endTime = Calendar.getInstance().timeInMillis,value = 0))
+                } }
+            }else if(activitesList[position].name == "Tecnost"){
+                quantitySingleton.addActivity(QuantityCategory(0, activitesList[position].name, "Tecnosti danas ispio",  Calendar.getInstance(),
+                        mutableListOf(),0,"L"))
+                runBlocking { withContext(Dispatchers.IO){
+                    quantityDb.insert(QuantityTable( name = activitesList[position].name, description = "Tecnosti danas ispio",  date = Calendar.getInstance().timeInMillis,
+                            properties = "",value = 0, unit= "L"))
+                } }
+            }
+
+            showToast(context, "Activity Added")
         }
     }
 
